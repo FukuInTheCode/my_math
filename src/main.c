f2753dd72522b1b28bddb564b64b79aa71fa32b1
FukuInTheCode <tomc.code@gmail.com>
2023-08-11 16:59:57 +0200

    exp and ln lade with some help because a i have some lazyness and it ue taylor series

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include "../includes/my.h"
 
-int main(int argc, char* argv[]) {
-    printf("%lld\n", my_Factorial(5));
+int main(int argc, char *argv[]) {
+    if(argc<2) return 84;
+    double x = atoi(argv[1]);
+    int n = atoi(argv[1])*3; // Number of terms in the Taylor series
+
+    double approx = my_ln(x, n);
+    double actual = log(x);
+
+    printf("Approximation: %lf\n", approx);
+    printf("Actual exp: %lf\n", actual);
+
     return 0;
 }
