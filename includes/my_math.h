4f1920b277f14eb098ad92b4de29fbe3b277d7c7
FukuInTheCode <tomc.code@gmail.com>
2023-08-11 17:02:20 +0200

    change

--- includes/my_math.h
+++ includes/my_math.h
@@ -1,9 +1,12 @@
 #pragma once
 #define MYMATHH
 
+#include <math.h>
+
 #define ll long long
 
 double dot_product(double* a, double* b, unsigned int size);
+
 double my_power(double x, unsigned int n);
 int my_randInt(int minN, int maxN);
 double my_randFloat(double minN, double maxN);
