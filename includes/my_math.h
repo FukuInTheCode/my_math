3fba3c6d3f2ef38b43660ae85c9983b997b741d7
FukuInTheCode <tomc.code@gmail.com>
2023-08-16 16:33:33 +0200

    rand cs

--- includes/my_math.h
+++ includes/my_math.h
@@ -8,8 +8,8 @@
 double dot_product(double* a, double* b, unsigned int size);
 
 double my_power(double x, unsigned int n);
-int my_randInt(int minN, int maxN);
-double my_randFloat(double minN, double maxN);
+int my_randint(int minN, int maxN);
+double my_randfloat(double minN, double maxN);
 unsigned ll my_factorial(const unsigned int n);
 double my_exp(double x, int n);
 double my_ln(double x, int n);
