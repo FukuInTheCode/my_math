93f7e34bf91045cbf584bc87f743c72d4ffdac0e
FukuInTheCode <tomc.code@gmail.com>
2023-08-25 22:08:20 +0200

    readd the dot_p

--- includes/my_math.h
+++ includes/my_math.h
@@ -31,6 +31,7 @@ double my_power(double x, unsigned int n);
 int my_randint(int minN, int maxN);
 double my_randfloat(double minN, double maxN);
 unsigned ll my_factorial(const unsigned int n);
+double my_dot_product(double* a, double* b, unsigned int size);
 double my_exp(double x, int n);
 double my_ln(double x, int n);
 double my_abs(double x);
