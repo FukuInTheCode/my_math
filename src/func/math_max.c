8bcd51e586cec482d6181b3e11f00a1d0262281d
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 22:14:46 +0200

    add a max function  for int

--- src/func/math_max.c
+++ src/func/math_max.c
@@ -14,6 +14,20 @@ double my_max(double *x, size_t size)
     return max;
 }
 
+double my_max_i(int *x, size_t size)
+{
+    if (x == NULL)
+        return 0.0;
+
+    double max = x[0];
+    for (size_t i = 1; i < size; i++) {
+        if (x[i] > max) {
+            max = x[i];
+        }
+    }
+    return max;
+}
+
 double my_max_between(double x1, double x2)
 {
     return (x1 > x2) ? x1 : x2;
